132878597a561406da67e5842c7a2f7981cdfe90
Austin C <47762009+acrumx@users.noreply.github.com>
2022-10-23 21:46:42 -0700

    Create Convert a Boolean to a String.py
    Link to the kata: https://www.codewars.com/kata/551b4501ac0447318f0009cd/

---
+++ Code Wars/Convert a Boolean to a String.py
@@ -0,0 +1,7 @@
+# Link to the kata: https://www.codewars.com/kata/551b4501ac0447318f0009cd/
+
+def boolean_to_string(b):
+    if b == True:
+        return 'True'
+    elif b == False:
+        return 'False'
