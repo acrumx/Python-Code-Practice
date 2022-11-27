71ab669bd589a283200cdbe7563fbc7a9fa68863
Austin C <47762009+acrumx@users.noreply.github.com>
2022-11-27 12:09:06 -0800

    Create Coding Exercise 50: is_palindrome.py

---
+++ The Modern Python 3 Bootcamp/Coding Exercise 50: is_palindrome.py
@@ -0,0 +1,11 @@
+'''
+is_palindrome('testing') # False
+is_palindrome('tacocat') # True
+is_palindrome('hannah') # True
+is_palindrome('robert') # False
+is_palindrome('amanaplanacanalpanama') # True
+'''
+
+def is_palindrome(i):
+    if i[::-1] == i: return True
+    return False
