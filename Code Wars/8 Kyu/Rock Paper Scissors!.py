5187cf6eaf16090e95a9753c31a7905675670069
Austin <47762009+acrumx@users.noreply.github.com>
2025-09-08 12:37:06 -0700

    Update and rename Code Wars/Rock Paper Scissors!.py to Code Wars/8 Kyu/Rock Paper Scissors!.py

--- Code Wars/8 Kyu/Rock Paper Scissors!.py
+++ Code Wars/8 Kyu/Rock Paper Scissors!.py
@@ -1,5 +1,5 @@
 # Link to the kata: https://www.codewars.com/kata/5672a98bdbdd995fad00000f/
-
+# 8 Kyu
 def rps(p1, p2):
     if ((p1 == 'rock' and p2 == 'scissors') or (p1 == 'paper' and p2 == 'rock')
     or (p1 == 'scissors' and p2 == 'paper')):
