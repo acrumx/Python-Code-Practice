cd28f07e6a94e3cf469bdb2e5d9ce3c3f49d3c3f
Austin <47762009+acrumx@users.noreply.github.com>
2025-09-08 12:36:23 -0700

    Update and rename Code Wars/Vowel Count to Code Wars/7 kyu/Vowel Count.py

--- Code Wars/7 kyu/Vowel Count.py
+++ Code Wars/7 kyu/Vowel Count.py
@@ -1,4 +1,5 @@
 # Link to the kata: https://www.codewars.com/kata/54ff3102c1bad923760001f3/solutions/python
+# 7 Kyu
 import re 
 def get_count(sentence):
     return len(re.findall(r'[aeiou]', sentence))
